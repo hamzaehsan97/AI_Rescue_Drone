2382f6e35d8dc52209d107a219453761053a0950
hamzaehsan97 <hamza.ehsan@lawrence.edu>
2020-03-07 18:01:21 -0600

    Removed commented code

--- drone/activate.py
+++ drone/activate.py
@@ -147,20 +147,7 @@ class FrontEnd(object):
 
 
 
-            # OLD CODE
-            # hsv = cv2.cvtColor(frame_read.frame, cv2.COLOR_BGR2HSV)
-            # lower_red = np.array([0,70,50])
-            # upper_red = np.array([10,255,255])
-            # mask = cv2.inRange(hsv, lower_red, upper_red)
-            # # red = cv2.bitwise_and(frame,frame, mask= mask)
-            # redcnts = cv2.findContours(mask.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[-2]
-            # #greyscale the frame
-            # grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            # #detect faces from grey frame
-            # faces = face.detectMultiScale(grey, 1.3, 5) 
-            # #detect upperbody from frame
-            # bodies = body.detectMultiScale(grey, scaleFactor=1.15, minNeighbors = 5, flags = cv2.CASCADE_SCALE_IMAGE )
-            
+               
             
             if process_frame == True:
 
@@ -200,96 +187,7 @@ class FrontEnd(object):
                 font = cv2.FONT_HERSHEY_DUPLEX
                 cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
-            #####OLD CODE
-            # forward = 0
-            # rotate = 0
-            
-            # #If faces are not available wait 4 seconds before staying still
-            # if self.auto == True:
-            #     if len(faces)  == 0:
-            #         face_null_timer = face_null_timer+1
-            #         if face_null_timer >= 120:
-            #             self.yaw_velocity = 0
-            #             self.for_back_velocity = 0
-            #             self.left_right_velocity = 0
-            #             self.up_down_velocity = 0
-                    
-                
-            # #If faces exist then overlay rectangles on the frame 
-            
-            # for (x,y,w,h) in faces:
-            #     face_null_timer = 0
-            #     rectangle = cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 2)
-            #     cv2.circle(frame, (480, 360), 5, (0,255,0), thickness=2)
-            #     xVal=int(x+(w/2))
-            #     yVal=int(y+(h/2))
-            #     circ = cv2.circle(frame,(xVal,yVal), 5, (255,0,0), 2)
-            #     roi_grey = grey[y:y+h, x:x+w]
-            #     roi_color = frame[y:y+h, x:x+w]
-            #     xDistance = xVal - 480
-            #     yDistance = yVal - 360
-
-            #     #If autonomous flight is set to true then fly towards the face in the frame
-            #     if(self.auto == True):
-            #         print("In the auto loop")
-            #         if(abs(xDistance) > 50 and abs(yDistance) > 50 and xVal,yVal != 0 and x,y != 0 and w,h == True):
-            #             print("Face detected, centering in")
-            #             # If subject is to the left or right
-            #             if(xDistance < horizontal_distance_face):
-            #                 self.yaw_velocity = int(-S)
-            #                 print("turn left")
-            #             elif(xDistance > horizontal_distance_face):
-            #                 self.yaw_velocity = int(S)
-            #                 print("turn right")
-            #             else:
-            #                 self.yaw_velocity = 0
-            #                 print("dont turn")
-            #             # If subject is above or below the frame    
-            #             if(yDistance < -130):
-            #                 self.up_down_velocity = int(S)
-            #                 print("Go up")
-            #             elif(yDistance > 130):
-            #                 self.up_down_velocity = int(-S)
-            #                 print("Go down")
-            #             else:
-            #                 self.up_down_velocity = 0
-            #                 print("Y constant")
-            #                 # If subject is above or below the frame    
-            #             if(w < 70):
-            #                 self.for_back_velocity = S
-            #                 print("Go forward")
-            #             elif(w > 150):
-            #                 self.for_back_velocity = -S
-            #                 print("Go back")
-            #             else:
-            #                 self.for_back_velocity = 0
-            #                 print("z constant")
-            # #if Bodies exist in the frame then draw rectangles around them
-            # for (x,y,w,h) in bodies:
-            #     bodyRectangle = cv2.rectangle(frame, (x,y), (x+w, y+h), (240,100,100), 1)
-            #     xVal=int(x+(w/2))
-            #     yVal=int(y+(h/2))
-            #     bodyCircle = cv2.circle(frame,(xVal,yVal), 5, (240,100,100), 1)
-
-            #     #if contour is available for red mask then draw a rectangle
-            #     if len(redcnts)>0:
-            #         redArea = max(redcnts, key=cv2.contourArea)
-            #         (xg,yg,wg,hg) = cv2.boundingRect(redArea)
-            #         length = math.sqrt((wg*wg)+(hg*hg))
-            #         xValue = int(xg+(wg/2))
-            #         yValue = int(yg+(hg/2))
-            #         xDist = xVal - xValue
-            #         yDist = yVal - yValue
-            #         distanceToBody = math.sqrt((xDist * xDist)+(yDist * yDist))
-            #         if length >= 150 and distanceToBody < 300:
-            #             cv2.rectangle(frame,(xg,yg),(xg+wg, yg+hg),(0,100,100),2)
-            #             shirtCircle = cv2.circle(frame,(xValue,yValue), 5, (0,100,100), 1)
-            #         elif len(faces)  > 0:
-            #             print("Found body")
-            #             addToDatabase(datetime.date,self.tello.get_attitude)
-                       
-                        
-
+           
             
             #Get drone battery information
             self.battery = self.tello.get_battery()  
